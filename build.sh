ca1672bd9af8b1de9ac914ed68f6e5cfe3da8cce
zhenjie zhu <tech@intellij.io>
2024-09-12 14:34:11 +0800

    Rename log messages for consistency
    Updated log messages in build.sh to maintain naming consistency with the actual build scripts. This change ensures clearer and more understandable log outputs.

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ cd "$SHELL_FOLDER"
 source <(curl -SL https://gitlab.com/iprt/shell-basic/-/raw/main/build-project/basic.sh)
 source <(curl -sSL $ROOT_URI/func/log.sh)
 
-log_info "build netty-socks-server"
+log_info "build netty-server-socks"
 bash netty-socks-server/build.sh
 
-log_info "build netty-tcp-server"
+log_info "build netty-server-tcp"
 bash netty-tcp-server/build.sh
